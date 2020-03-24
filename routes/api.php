eaa1fe0fb03653b25db399fe024086e0496a00bf
artemhales <od110894hav@gmail.com>
2020-03-24 10:33:24 +0200

    create api route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,62 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::prefix('/freelancer')->group(function () {
+
+    // роут получает всех фрилансеров
+    Route::get
+    ('/', ['uses' => 'FreelanceController@getFreelances']);
+    // в будущем получает имя фрилансера
+    Route::get
+    ('/{freelance_name}', ['uses' => 'FreelanceController@get']);
+    // создание фрилансера
+    Route::post
+    ('/', ['uses' => 'FreelanceController@create']);
+    // удаление фрилансера
+    Route::delete
+    ('/{freelancer_id}', ['uses' => 'FreelanceController@delete'])->where(['freelancer_id' => '[0-9+]']);
+    // обновить запись
+    Route::put('/{freelancer_id}', ['uses' => 'FreelanceController@update'])->where(['freelancer_id' => '[0-9+]']);
+});
+
+Route::prefix('/customers')->group(function () {
+
+    Route::get
+    ('/', ['uses' => 'CustomerController@get']);
+    Route::get
+    ('/{customer_name}', ['uses' => 'CustomerController@get']);
+    Route::post
+    ('/', ['uses' => 'CustomerController@create']);
+    Route::delete
+    ('/{customer_id}', ['uses' => 'CustomerController@delete'])->where(['customer_id' => '[0-9+]']);
+    Route::put('/{customer_id}', ['uses' => 'CustomerController@update'])->where(['customer_id' => '[0-9+]']);
+});
+
+Route::prefix('/orders')->group(function () {
+
+    Route::get
+    ('/', ['uses' => 'OrderController@get']);
+    Route::get
+    ('/{order_name}', ['uses' => 'OrderController@get']);
+    Route::post
+    ('/', ['uses' => 'OrderController@create']);
+    Route::delete
+    ('/{order_id}', ['uses' => 'OrderController@delete'])->where(['order_id' => '[0-9+]']);
+    Route::put('/{order_id}', ['uses' => 'OrderController@update'])->where(['order_id' => '[0-9+]']);
+});
+
+Route::prefix('/applications')->group(function () {
+
+    Route::get
+    ('/', ['uses' => 'ApplicationController@get']);
+    Route::get
+    ('/{application_name}', ['uses' => 'ApplicationController@get']);
+    Route::post
+    ('/', ['uses' => 'ApplicationController@create']);
+    Route::delete
+    ('/{application_id}', ['uses' => 'ApplicationController@delete'])->where(['application_id' => '[0-9+]']);
+    Route::put('/{application_id}', ['uses' => 'ApplicationController@update'])->where(['application_id' => '[0-9+]']);
 });
